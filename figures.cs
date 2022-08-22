7456d15e4369357183996408c6d3e7f71e64c134
KT815G <111741892+KT815G@users.noreply.github.com>
2022-08-22 13:08:32 +0300

    git commit

--- figures.cs
+++ figures.cs
@@ -1,4 +1,17 @@
-public class Figures{
-
-
+public class Figure{
+    public string Type;
+    public int Layer;
+    public int X, Y;
 }
+
+public class Rectangle: Figure{
+    public int Width, Height;
+    public Rectangle(int X, int Y, int Width, int Height){
+        this.X=X;
+        this.Y=Y;
+        this.Width=Width;
+        this.Height=Height;
+    }
+}
+
+
